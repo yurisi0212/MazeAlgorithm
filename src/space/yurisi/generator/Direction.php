298e497565c45c6dc3450f0c446cc29ad8f1e781
yurisi0212 <Dev.yurisi@gmail.com>
2022-04-04 18:29:48 +0900

    完成

---
+++ src/space/yurisi/generator/Direction.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace space\yurisi\generator;
+
+interface Direction{
+    public const Up = 0;
+    public const Right = 1;
+    public const Down = 2;
+    public const Left = 3;
+}
--- src/space/yurisi/generator/MazeGenerator.php
+++ src/space/yurisi/generator/MazeGenerator.php
@@ -7,11 +7,19 @@ use pocketmine\block\Block;
 use pocketmine\block\BlockFactory;
 use pocketmine\block\BlockLegacyIds;
 use pocketmine\player\Player;
+use pocketmine\world\Position;
+use pocketmine\world\World;
 
 class MazeGenerator {
 
     private Block $wall;
     private Block $road;
+    private Block $side;
+
+    private Position $position;
+    private World $level;
+
+    private array $start = [];
 
     public function __construct(
         private int    $x,
@@ -19,8 +27,11 @@ class MazeGenerator {
         private int    $z,
         private Player $player
     ) {
-        $this->wall = BlockFactory::getInstance()->get(BlockLegacyIds::OBSIDIAN, 0);
-        $this->road = BlockFactory::getInstance()->get(BlockLegacyIds::STONE, 0);
+        $this->wall = BlockFactory::getInstance()->get(BlockLegacyIds::STONE, 0);
+        $this->road = BlockFactory::getInstance()->get(BlockLegacyIds::AIR, 0);
+        $this->side = BlockFactory::getInstance()->get(BlockLegacyIds::OBSIDIAN, 0);
+        $this->position = $this->player->getPosition();
+        $this->level = $this->position->getWorld();
     }
 
     public function generate() {
@@ -29,19 +40,106 @@ class MazeGenerator {
         if ($width % 2 === 0) $width++;
         if ($height % 2 === 0) $height++;
 
-        $position = $this->player->getPosition();
+        $this->setUp($width, $height);
+        $this->dig(1, 1);
+        $this->makeSideWall($width, $height);
+    }
+
+    private function setUp(int $width, int $height){
         for ($z = 0; $z < $height; $z++) {
             for ($x = 0; $x < $width; $x++) {
                 for ($y = 0; $y < $this->y; $y++) {
-                    $pos = clone $position->add($x, $y, $z);
+                    $pos = $this->position->add($x, $y, $z);
                     if ($x == 0 || $z == 0 || $x == $width - 1 || $z == $height - 1) {
-                        $position->getWorld()->setBlock($pos->asVector3(), $this->wall);
-                    }else{
-                        $position->getWorld()->setBlock($pos->asVector3(), $this->road);
+                        $this->level->setBlock($pos->asVector3(), $this->road);
+                    } else {
+                        $this->level->setBlock($pos->asVector3(), $this->wall);
                     }
                 }
             }
         }
     }
+
+    private function dig(int $x, int $z) {
+        $position = $this->position;
+        $level = $this->level;
+        while (true) {
+            $direction = [];
+            if ($level->getBlock($position->add($x, 0, $z - 1))->getId() === $this->wall->getId() && $level->getBlock($position->add($x, 0, $z - 2))->getId() === $this->wall->getId()) {
+                $direction[] = Direction::Up;
+            }
+            if ($level->getBlock($position->add($x + 1, 0, $z))->getId() === $this->wall->getId() && $level->getBlock($position->add($x + 2, 0, $z))->getId() === $this->wall->getId()) {
+                $direction[] = Direction::Right;
+            }
+            if ($level->getBlock($position->add($x, 0, $z + 1))->getId() === $this->wall->getId() && $level->getBlock($position->add($x, 0, $z + 2))->getId() === $this->wall->getId()) {
+                $direction[] = Direction::Down;
+            }
+            if ($level->getBlock($position->add($x - 1, 0, $z))->getId() === $this->wall->getId() && $level->getBlock($position->add($x - 2, 0, $z))->getId() === $this->wall->getId()) {
+                $direction[] = Direction::Left;
+            }
+
+            if (count($direction) === 0) break;
+
+            switch ($direction[mt_rand(0, count($direction) - 1)]) {
+                case Direction::Up:
+                    $this->setYPosition($x, --$z);
+                    $this->setYPosition($x, --$z);
+                    break;
+                case Direction::Right:
+                    $this->setYPosition(++$x, $z);
+                    $this->setYPosition(++$x, $z);
+                    break;
+                case Direction::Down:
+                    $this->setYPosition($x, ++$z);
+                    $this->setYPosition($x, ++$z);
+                    break;
+                case Direction::Left:
+                    $this->setYPosition(--$x, $z);
+                    $this->setYPosition(--$x, $z);
+                    break;
+            }
+        }
+        $pos = $this->getStartPos();
+        if ($pos != null) $this->dig($pos->getFloorX(), $pos->getFloorZ());
+    }
+
+    private function setYPosition(int $x, int $z){
+        for ($y = 0; $y < $this->y; $y++) {
+            $this->setAir($x, $y, $z);
+        }
+    }
+
+    private function setAir(int $x, int $y, int $z) {
+        $this->level->setBlock($this->position->add($x, $y, $z), $this->road);
+        if ($x % 2 == 1 && $z % 2 == 1) {
+            if (!in_array(new Position($x, $this->y, $z, $this->level), $this->start)) {
+                $this->start[] = new Position($x, $this->y, $z, $this->level);
+            }
+        }
+    }
+
+    private function getStartPos(): ?Position {
+        if (count($this->start) == 0) return null;
+
+        $num = mt_rand(0, count($this->start) - 1);
+        $pos = $this->start[$num];
+        unset($this->start[$num]);
+        $this->start = array_values($this->start);
+        return $pos;
+    }
+
+    private function makeSideWall(int $width, int $height) {
+        for ($z = 0; $z < $height; $z++) {
+            for ($x = 0; $x < $width; $x++) {
+                for ($y = 0; $y < $this->y; $y++) {
+                    $pos = $this->position->add($x, $y, $z);
+                    if ($x == 0 || $z == 0 || $x == $width - 1 || $z == $height - 1) {
+                        $this->level->setBlock($pos->asVector3(), $this->side);
+                    }
+                }
+            }
+        }
+    }
+
 
 }
